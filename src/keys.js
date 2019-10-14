f9b9ea5e7b717abafeaf01188d0891985cf72af3
superyaz <yacamocursos@hotmail.com>
2019-10-14 00:11:51 -0500

    Se configura la conexión a la base de datos en heroku

--- src/keys.js
+++ src/keys.js
@@ -12,11 +12,13 @@
 module.exports = {
     database: {
         host: 'us-cdbr-iron-east-05.cleardb.net',
-        user: 'b32ac971ae2d5e',
-        password: '84e253a5',
-        database: 'heroku_bd7eac19184614a'
+        user: 'b734ec800bb78f',
+        password: 'ac8619ea',
+        database: 'heroku_9c05c4e47d27c50'
     }
 };
 
 
 // mysql://b32ac971ae2d5e:84e253a5@us-cdbr-iron-east-05.cleardb.net/heroku_bd7eac19184614a?reconnect=true
+
+//mysql://b734ec800bb78f:ac8619ea@us-cdbr-iron-east-05.cleardb.net/heroku_9c05c4e47d27c50?reconnect=true
